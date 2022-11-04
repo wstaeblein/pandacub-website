34751dc1d691bc8198192c7e98b63a944d5c6f4e
Walter Staeblein <walter@synergys.com.br>
2022-11-04 01:07:26 -0300

    Teste functions

--- functions/version.js
+++ functions/version.js
@@ -1,12 +1,13 @@
 exports.version = async function (event, context) {
 
-    if (event.httpMethod == 'GET') {
+/*     if (event.httpMethod == 'GET') {
         
     }
     var version = require('../package.json').version;
-    var beta = version[0] == '0' ? ' Beta' : '';
+    var beta = version[0] == '0' ? ' Beta' : ''; */
     return {
         statusCode: 200,
-        body: JSON.stringify({ version: version + beta })
+        //body: JSON.stringify({ version: version + beta })
+        body: JSON.stringify({ version: '0.7.7' })
     };
 };
